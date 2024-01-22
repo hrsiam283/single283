fc11ac187c3ce1dea81db0440a4b93257a621076
hafiz <hafiz@gmail>
2024-01-22 15:48:05 +0600

    three

--- single.java
+++ single.java
@@ -1,3 +1,9 @@
 public interface Shape{
     void draw();
 }
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
